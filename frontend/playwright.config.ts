91c14bf563eb0218b5ef018dfe69411a0acb505a
suphutsorn <suphutsorn092003@gmail.com>
2025-09-09 12:39:40 +0700

    ทดสอบ test

--- frontend/playwright.config.ts
+++ frontend/playwright.config.ts
@@ -1,13 +1,14 @@
 import { defineConfig } from '@playwright/test';
 
 export default defineConfig({
-  testDir: './tests',       // เก็บ test case ทั้งหมดในโฟลเดอร์ tests
+  testDir: './tests',
   timeout: 30 * 1000,
-  reporter: [['html', { open: 'never' }]], // เพิ่มตรงนี้
+  reporter: [['html', { open: 'never' }]],
   use: {
-    headless: false,        // false = เห็น browser จริง เหมาะกับ UAT
-    baseURL: 'http://localhost:5173/', // vite dev server ปกติรันที่ 5173
+    headless: false,
+    baseURL: 'http://localhost:5173/',
     screenshot: 'on',
-    // video: 'retain-on-failure',
+    storageState: 'tests/auth.json',
   },
+  globalSetup: './global-setup.ts', // <-- ใส่ path string
 });
