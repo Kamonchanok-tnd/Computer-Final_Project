3f33ad12308fcef9549c3268e831f4c498ad05f4
suphutsorn <suphutsorn092003@gmail.com>
2025-09-09 12:51:21 +0700

    merge ปลื้ม

--- frontend/playwright.config.ts
+++ frontend/playwright.config.ts
@@ -2,16 +2,7 @@
 import { defineConfig } from '@playwright/test';
 
 export default defineConfig({
-
-  testDir: './tests',
-  timeout: 30 * 1000,
   reporter: [['html', { open: 'never' }]],
-  use: {
-    headless: false,
-    baseURL: 'http://localhost:5173/',
-    screenshot: 'on',
-    storageState: 'tests/auth.json',
-  },
   globalSetup: './e2e/global-setup.ts',
 
   projects: [
@@ -52,5 +43,4 @@ export default defineConfig({
       },
     },
   ],
-
 });
